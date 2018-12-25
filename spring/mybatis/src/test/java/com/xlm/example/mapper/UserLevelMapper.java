7651117d9bdfd9a95e76ce79045bb13a618b735e
assembly <273300540@qq.com>
2018-12-25 11:03:54 +0800

    Delete UserLevelMapper.java

--- spring/mybatis/src/test/java/com/xlm/example/mapper/UserLevelMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xlm.example.mapper;
-
-import org.apache.ibatis.annotations.Update;
-
-
-public interface UserLevelMapper  {
-    @Update("insert into userlevel(id) value(#{param1})")
-    void insertLevel(Integer[] array);
-}
